330e3a71c0d0015251037033998f4dcc967d2efb
tracer-ics2021 <tracer@njuics.org>
2022-10-09 17:15:48 +0800

    >  compile 201220000

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -1,6 +1,5 @@
 #include <isa.h>
 #include "local-include/reg.h"
-
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -9,6 +8,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
