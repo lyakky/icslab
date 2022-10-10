1cc2d2cf57da11036180d946a781185585aeffa3
tracer-ics2021 <tracer@njuics.org>
2022-10-10 20:32:13 +0800

    >  compile 201220000

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -92,7 +92,7 @@ static int cmd_memory(char *args)
   for (size_t i = 0; i < len; i++)
   {
     word_t v = vaddr_read(loc + 4 * i, 4);
-    printf("loc:%08lx, memval:%08lx\n", loc + i * 4, v);
+    printf("loc:%#08lx, memval:%#08lx\n", loc + i * 4, v);
   }
 
   printf("\n");
